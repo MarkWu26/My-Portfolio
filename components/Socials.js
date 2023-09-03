231c811bb938e7cb8312f95a1b66ced4e3eedc10
MarkWu26 <markwu1230@gmail.com>
2023-09-03 22:01:32 +0800

    updated social links

--- components/Socials.js
+++ components/Socials.js
@@ -5,19 +5,19 @@ import Link from 'next/link'
 
 const Socials = () => {
   return <div className='flex items-center gap-x-5 text-lg'>
-  <Link href={'https://github.com/MarkWu26/'} className="hover:text-accent transition-all duration-300">
+  <a href={'https://github.com/MarkWu26/'} target="_blank" className="hover:text-accent transition-all duration-300">
     <RiGithubLine/>
-  </Link>
-  <Link href={''} className="hover:text-accent transition-all duration-300">
+  </a>
+  <a href={'https://www.linkedin.com/in/markwu30/'} target="_blank" className="hover:text-accent transition-all duration-300">
     <RiLinkedinLine/>
-  </Link>
+  </a>
   <Link href={''} className="hover:text-accent transition-all duration-300">
     <RiInstagramLine/>
   </Link>
-  <Link href={''} className="hover:text-accent transition-all duration-300">
+  <a href={'https://www.facebook.com/markstanley.wu'} target="_blank" className="hover:text-accent transition-all duration-300">
     <RiFacebookLine/>
-  </Link>
-  <Link href={''} className="hover:text-accent transition-all duration-300">
+  </a>
+  <Link href={'https://www.youtube.com/channel/UC6KL3Iti9rf2p2vgMyfpk3Q'}  target="_blank" className="hover:text-accent transition-all duration-300">
     <RiYoutubeLine/>
   </Link>
   </div>;
